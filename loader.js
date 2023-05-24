913c90d5ab110da114c3149e9aa1b9fbb79f0cae
Abrahan Pulgar <128384748+zirniko@users.noreply.github.com>
2023-05-24 12:37:17 -0400

    Create loader.js
    add loader js archivo

---
+++ loader.js
@@ -0,0 +1,5 @@
+window.addEventListener("load", function() {
+  // Eliminar el preloader cuando la página se haya cargado completamente
+  var preloader = document.querySelector(".preloader");
+  preloader.style.display = "none";
+});
