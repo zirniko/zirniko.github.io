685a547b639d81b9acbef1c6df0b9bfb20a0b065
Abrahan Pulgar <128384748+zirniko@users.noreply.github.com>
2023-06-01 12:53:23 -0400

    Update and rename loader.js to script.js
    rename y codigo nnuevo del script

--- loader.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener("load", function() {
-  // Eliminar el preloader cuando la página se haya cargado completamente
-  var preloader = document.querySelector(".preloader");
-  preloader.style.display = "none";
-});
