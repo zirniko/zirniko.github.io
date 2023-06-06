c8ea136db68dcd09d49cffe5d42c840a11630661
Abrahan Pulgar <128384748+zirniko@users.noreply.github.com>
2023-06-06 09:45:40 -0400

    Update script.js
    update js

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+window.onload = function() {
+  gsap.fromTo(
+    ".loading-page",
+    { opacity: 1 },
+    {
+      opacity: 0,
+      display: "none",
+      duration: 1.5,
+      delay: 3.5,
+    }
+  );
+
+  gsap.fromTo(
+    ".logo-name",
+    {
+      y: 50,
+      opacity: 0,
+    },
+    {
+      y: 0,
+      opacity: 1,
+      duration: 2,
+      delay: 0.5,
+    }
+  );
+};
+
+/*
 gsap.fromTo(
   ".loading-page",
   { opacity: 1 },
@@ -22,3 +50,4 @@ gsap.fromTo(
     delay: 0.5,
   }
 );
+*/
