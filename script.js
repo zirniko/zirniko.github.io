852f6ce06c3d7e4ac60cf9929a57fbf9f3bad6e4
Abrahan Pulgar <128384748+zirniko@users.noreply.github.com>
2023-06-06 15:47:05 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,3 @@
-window.onload = function() {
-  gsap.fromTo(
-    ".loading-page",
-    { opacity: 1 },
-    {
-      opacity: 0,
-      display: "none",
-      duration: 1.5,
-      delay: 3.5,
-    }
-  );
-
-  gsap.fromTo(
-    ".logo-name",
-    {
-      y: 50,
-      opacity: 0,
-    },
-    {
-      y: 0,
-      opacity: 1,
-      duration: 2,
-      delay: 0.5,
-    }
-  );
-};
-
-/*
 gsap.fromTo(
   ".loading-page",
   { opacity: 1 },
@@ -50,4 +22,3 @@ gsap.fromTo(
     delay: 0.5,
   }
 );
-*/
