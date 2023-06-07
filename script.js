c9816cc0f1042f32872c178e4bd3e485fd8a5bb7
Abrahan Pulgar <128384748+zirniko@users.noreply.github.com>
2023-06-07 11:11:02 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,5 @@
-gsap.fromTo(
-  ".loading-page",
-  { opacity: 1 },
-  {
-    opacity: 0,
-    display: "none",
-    duration: 1.5,
-    delay: 3.5,
-  }
-);
-
-gsap.fromTo(
-  ".logo-name",
-  {
-    y: 50,
-    opacity: 0,
-  },
-  {
-    y: 0,
-    opacity: 1,
-    duration: 2,
-    delay: 0.5,
-  }
-);
+window.addEventListener("load", function() {
+    var preloader = document.getElementById("preloader");
+    preloader.style.display = "none"; // Oculta el preloader
+  });
+  
