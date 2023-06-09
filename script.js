fe03c99db6aaeda40b0da104993aa4d9cff20a26
Abrahan Pulgar <128384748+zirniko@users.noreply.github.com>
2023-06-08 20:48:56 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
+/*principal
 window.addEventListener("load", function() {
     var preloader = document.getElementById("preloader");
     preloader.style.display = "none"; // Oculta el preloader
   });
-  
+  */
+window.addEventListener("load", function() {
+  var preloader = document.getElementById("preloader");
+  preloader.classList.add("preloader-hidden");
+});
