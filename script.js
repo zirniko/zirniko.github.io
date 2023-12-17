13d65807845b87321b3de9578660cf56baf9ea5f
Abrahan Pulgar <128384748+zirniko@users.noreply.github.com>
2023-12-16 22:06:13 -0300

    Update script.js
    script loader update

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-window.addEventListener("load", function() {
+/*window.addEventListener("load", function() {
     var preloader = document.getElementById("preloader");
     preloader.style.display = "none"; // Oculta el preloader
   });
+*/
 
+document.addEventListener('DOMContentLoaded', function () {
+  // Muestra el contenedor principal
+  document.getElementById('page-container').style.display = 'block';
 
+  // Oculta el loader
+  document.getElementById('loader').style.display = 'none';
+});
